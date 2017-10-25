ca5428cb018fce2057bd22932e02f85f1327028d
Patrick <bleowy1@gmail.com>
2017-10-26 00:12:29 +0200

    Initial commit

---
+++ desktop/src/pl/bbl/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.bbl.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.bbl.game.BlocksGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlocksGame(), config);
+	}
+}
