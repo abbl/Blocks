a571baa13fdf5d314c7fd82a18f0eacf84724f68
Patrick <bleowy1@gmail.com>
2017-10-27 23:37:51 +0200

    savin stuff

--- core/src/pl/bbl/game/entities/GameObject.java
+++ core/src/pl/bbl/game/entities/GameObject.java
@@ -1,4 +1,27 @@
 package pl.bbl.game.entities;
 
-public class GameObject {
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class GameObject {
+    private Vector3 position;
+
+    protected GameObject(){
+        position = new Vector3();
+    }
+
+    public void update(){
+
+    }
+
+    public void render(){
+
+    }
+
+    public Vector3 getPosition(){
+        return position;
+    }
+
+    public void setPosition(Vector3 position){
+        this.position = position;
+    }
 }
