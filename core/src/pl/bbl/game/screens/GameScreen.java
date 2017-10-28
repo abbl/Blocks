8c14aaa758c74dbed6c67a6b9d5ef9b37b783806
Patrick <bleowy1@gmail.com>
2017-10-28 02:35:03 +0200

    Added some basics screens.

---
+++ core/src/pl/bbl/game/screens/GameScreen.java
@@ -0,0 +1,21 @@
+package pl.bbl.game.screens;
+
+import com.badlogic.gdx.Game;
+import pl.bbl.game.screens.abstraction.ThreeDimensionalAbstractScreen;
+
+public class GameScreen extends ThreeDimensionalAbstractScreen {
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    protected void renderModels() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+}
